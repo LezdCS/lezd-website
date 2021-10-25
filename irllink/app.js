dfadc1d30b139648d9c621b0797e84f7b11b2aab
Lezd <lilinouxgaming@gmail.com>
2021-10-26 00:59:56 +0200

    express twitchauth redirection

--- irllink/app.js
+++ irllink/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const url = require('url');
 const app = express()
 
+app.get('/twitchauth', (req,res) => {
 
-app.get('/parkings', (req,res) => {
-    res.send("Liste des parkings")
+    res.status(301).redirect(url.format({
+        pathname:"http://localhost/",
+        query: {
+            "access_token:":req.query.access_token
+        }
+    }));
+
 })
-
 
 app.listen()
 
