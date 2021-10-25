dc5b21b62dbeeda8923f51cd4d8cbb43510d7900
Lezd <lilinouxgaming@gmail.com>
2021-10-26 00:15:55 +0200

    express rest

--- irllink/app.js
+++ irllink/app.js
@@ -1,13 +1,11 @@
-var http = require('http');
-var wiki = require('./wiki.js');
+const express = require('express')
+const app = express()
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
 
-server.use('/wiki', wiki);
+app.get('/parkings', (req,res) => {
+    res.send("Liste des parkings")
+})
+
+
+app.listen()
+
