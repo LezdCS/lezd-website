3ac745c1c3d8f28c10a503571628a497a070c684
Lezd <lilinouxgaming@gmail.com>
2021-10-26 20:17:05 +0200

    improving express routes

--- irllink/app.js
+++ irllink/app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const url = require('url');
+const path = require('path')
 const app = express()
 
 app.get('/twitchauth', (req,res) => {
 
+    res.sendFile(path.join(__dirname+'/index.html'))
+
+})
+
+app.get('/token', (req,res) => {
+
     res.status(301).redirect(url.format({
-        pathname:"http://localhost/",
+        pathname:"dev.lezd.www.irllink:/",
         query: {
             "access_token:":req.query.access_token
         }
