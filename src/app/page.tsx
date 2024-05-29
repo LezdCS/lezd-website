8274951e70ba4c3826df14519f094174e7ac769d
LezdCS <xxeurobluexx@gmail.com>
2024-05-29 18:34:20 +0900

    remove nextseo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 'use client'
 
 import classes from "./page.module.scss";
-import { NextSeo } from "next-seo";
 
 export default function Home() {
   return (
     <div className={classes.Page}>
-      <NextSeo
-        title="Home Page Title"
-        description="Home page description of the page"
-      />
       
     </div>
   );
